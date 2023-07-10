e9c714621751d9a603d1417eae1d940d70cecc8e
Alesia <alesia.litvinenko@gmail.com>
2023-07-10 14:29:31 +0300

    feat: add style in project

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,12 +5,12 @@ const App: React.FC = () => {
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">123313</div>
+        <div className="current-operand">133488</div>
       </div>
 
       {BUTTON.map((el, i) => {
-        if (i === 0 || i === BUTTON.length) {
+        if (i === 0 || i === BUTTON.length - 1) {
           return <button className="span-two">{el}</button>;
         }
         return <button>{el}</button>;
