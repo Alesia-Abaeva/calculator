c4c257166d599e55ba1113e5afbdc9b386175906
Alesia <alesia.litvinenko@gmail.com>
2023-07-10 14:14:01 +0300

    feat: add main section

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,4 @@
+import { BUTTON } from 'shared/const/button';
 import './App.css';
 
 const App: React.FC = () => {
@@ -7,8 +8,13 @@ const App: React.FC = () => {
         <div className="previous-operand"></div>
         <div className="current-operand"></div>
       </div>
-      <button className="span-two">AC</button>
-      <button className="span-two">DEL</button>
+
+      {BUTTON.map((el, i) => {
+        if (i === 0 || i === BUTTON.length) {
+          return <button className="span-two">{el}</button>;
+        }
+        return <button>{el}</button>;
+      })}
     </div>
   );
 };
